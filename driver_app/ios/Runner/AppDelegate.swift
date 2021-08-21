83cff18679bec6833c845d6256883ee4999944c8
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2021-08-21 21:17:16 +0700

    Driver App - Set google map | Geolocator services

--- driver_app/ios/Runner/AppDelegate.swift
+++ driver_app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyA_HHbbQ8j4WKl62RNGbPR7QmAM5-IPz3s")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- driver_app/lib/tabsPages/home_tab_page.dart
+++ driver_app/lib/tabsPages/home_tab_page.dart
@@ -1,12 +1,61 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
-class HomeTabPage extends StatelessWidget {
-  const HomeTabPage({ Key? key }) : super(key: key);
+class HomeTabPage extends StatefulWidget {
+  const HomeTabPage({Key? key}) : super(key: key);
+
+  @override
+  _HomeTabPageState createState() => _HomeTabPageState();
+}
+
+class _HomeTabPageState extends State<HomeTabPage> {
+  Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  GoogleMapController? newGoogleMapController;
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  Position? currentPosition;
+  var geolocator = Geolocator();
+
+  void locatePosition() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    currentPosition = position;
+
+    LatLng latLngPosition = LatLng(position.latitude, position.longitude);
+
+    CameraPosition cameraPosition =
+        CameraPosition(target: latLngPosition, zoom: 14);
+    newGoogleMapController
+        ?.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+    // String address =
+    //     await AssistantMethods.searchCoordinateAddress(position, context);
+    // print("{ THIS IS YOUR ADDRESS $address }");
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("This is Home Tab Page"),
+    return Stack(
+      children: [
+        GoogleMap(
+          mapType: MapType.normal,
+          myLocationButtonEnabled: true,
+          myLocationEnabled: true,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controllerGoogleMap.complete(controller);
+            newGoogleMapController = controller;
+
+            locatePosition();
+          },
+        ),
+      ],
     );
   }
 }
